dc9358ed33d6c4d91c0a12bd3072a3e5c1c90ad4
jmanrocks152 <jmanrocks152@gmail.com>
2017-05-23 15:20:20 -0500

    What we think to be the correct solution as of yet, will need to be tested

--- .idea/src/Main.java
+++ .idea/src/Main.java
@@ -12,53 +12,64 @@ public class Main {
         //Variable initialization
         Scanner scanner = new Scanner(System.in);
         int testCases = scanner.nextInt();
-        String nqString = scanner.nextLine();
         String delims = "[ ]+";
-        String[] nqArray = nqString.split(delims);
-        int numberOfSnakes, queries, currentQuery, numberOfPassingSnakes = 0;
+        int numberOfSnakes, queries, currentQuery, largestSnakeLength = 0, numberOfPassingSnakes = 0;
         int arrayListOffset = 0;
-        String snakeLengthsString = scanner.nextLine();
-        String[] snakeLengthsArray = snakeLengthsString.split(delims);
         ArrayList<Integer> nqArrayList = new ArrayList<>();
         ArrayList<Integer> snakeLengthsArrayList = new ArrayList<>();
         ArrayList<Integer> results = new ArrayList<>();
 
-        //For loop that adds the values of nqArray to nqArrayList and sets the respective variables to their values
-        for(String s : nqArray) {
-            nqArrayList.add(Integer.parseInt(s));
-        }
-
-        numberOfSnakes = nqArrayList.get(0);
-        queries = nqArrayList.get(1);
-        //Ditto
-        for(String s : snakeLengthsArray) {
-            nqArrayList.add(Integer.parseInt(s));
-        }
-
-        //For loop that ensures the program only activates for as many test cases there are
         for(int i = 0; i < testCases; i++) {
-            for(int x = 0; x < queries; x++) {
-                currentQuery = scanner.nextInt();
-                for(int s : snakeLengthsArrayList) {
-                    if(s >= currentQuery) {
-                        numberOfPassingSnakes++;
-                        snakeLengthsArrayList.remove(s - arrayListOffset);
-                        arrayListOffset++;
-                    }
+            String nqString = scanner.nextLine();
+            String[] nqArray = nqString.split(delims);
+            String snakeLengthsString = scanner.nextLine();
+            String[] snakeLengthsArray = snakeLengthsString.split(delims);
 
-
-                }
-                for(int z = 0; z < snakeLengthsArrayList.size(); z++) {
-                    //To do: add code that checks if the largest snake still remaining can get enough to pass the query
-                }
+            for(String s : nqArray) {
+                nqArrayList.add(Integer.parseInt(s));
             }
-            currentQuery = 0;
-            //Need to add another way to input a second test case
+
+            numberOfSnakes = nqArrayList.get(0);
+            queries = nqArrayList.get(1);
+
+            for(String s : snakeLengthsArray) {
+                snakeLengthsArrayList.add(Integer.parseInt(s));
+            }
+
+            //For loop that ensures the program only activates for as many test cases there are
+
+                for(int x = 0; x < queries; x++) {
+                    currentQuery = scanner.nextInt();
+                    for(int s : snakeLengthsArrayList) {
+                        if(s >= currentQuery) {
+                            numberOfPassingSnakes++;
+                            snakeLengthsArrayList.remove(s - arrayListOffset);
+                            arrayListOffset++;
+                        }
+
+
+                    }
+                    for(int z = 0; z < snakeLengthsArrayList.size(); z++) {
+                        if(z == 0) {
+                            largestSnakeLength = snakeLengthsArrayList.get(0);
+                        }
+                        if(snakeLengthsArrayList.get(z) > largestSnakeLength) {
+                            largestSnakeLength = snakeLengthsArrayList.get(z);
+                        }
+                        if(largestSnakeLength + (snakeLengthsArrayList.size() - 1) > currentQuery) {
+                            numberOfPassingSnakes++;
+                        }
+                    }
+                    results.add(numberOfPassingSnakes);
+                }
+                currentQuery = 0;
+                arrayListOffset = 0;
+                numberOfPassingSnakes = 0;
+            }
+
+            for(int i = 0; i < results.size(); i++) {
+                System.out.println(results.get(i));
+                System.out.println(results.get(i));
+            }
         }
-
-
-
-
-
     }
-}
